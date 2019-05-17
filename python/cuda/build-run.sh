a3a68dbe947278b7c44831484515dfe9f80a56db
Jaypy.Hwang <sealooks@gmail.com>
2019-05-17 17:03:26 +0900

    Fixed trivial bugs.

--- python/cuda/build-run.sh
+++ python/cuda/build-run.sh
@@ -9,16 +9,14 @@ else
 fi
 
 # Add repeatedly git repo =====================
-
 export TARGET="coding-environment-test"
 export SOURCE="https://github.com/jaypy-h/coding-environment-test.git"
 if [ -d $TARGET ]
 then 
     cd $TARGET && git pull && cd ../../
 else
-    git clone $SOURCE $TARGET && cd ../../
+    git clone $SOURCE $TARGET && cd ../
 fi
-
 #==============================================
 
 export TAG="python-cuda"
