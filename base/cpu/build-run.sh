3c025d6e36292b5b413091bd171c50baad1d14e0
jaypy <sealooks@gmail.com>
2019-05-18 00:06:30 +0900

    Backup

--- base/cpu/build-run.sh
+++ base/cpu/build-run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 TAG="ubuntu-cpu"
-docker build -t coding-base-env:$TAG .
+docker build \
+    --build-arg USER="${USER}" \
+    --build-arg PASSWORD="${PASSWORD:-${USER}1234}" \
+    --build-arg HOME="${HOME:-/${USER}}" \
+    -t coding-base-env:latest \
+    -t coding-base-env:$TAG \
+    .
 
 docker run --rm -it -h ubuntu-cpu coding-base-env:$TAG /bin/zsh
--- python/cpu/build-run.sh
+++ python/cpu/build-run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export USER="ubuntu"
+export PASSWORD="${USER}1234"
+export HOME="/${USER}"
+
+# Check projects directory
 export PROJECTS="projects"
 if [ -d $PROJECTS ]
 then
@@ -10,7 +15,7 @@ fi
 
 # Add repeatedly git repo =====================
 export TARGET="coding-environment-test"
-export SOURCE="https://github.com/jaypy-h/coding-environment-test.git"
+export SOURCE="git@github.com:jaypy-h/coding-environment.git"
 if [ -d $TARGET ]
 then 
     cd $TARGET && git pull && cd ../../
@@ -23,4 +28,29 @@ export TAG="python-cpu"
 docker build -t coding-python-env:$TAG .
 
 export JUPYTER_PORT=8080
-docker run --rm -it -h python-cpu -p $JUPYTER_PORT:8080 coding-python-env:$TAG /bin/zsh
+
+if [ -e $SSH_KEY_PATH ]
+then
+    docker run --rm -it \
+        -h python-cpu \
+        -v $SSH_KEY_PATH:/ubuntu/.ssh/id_rsa \
+        -p $JUPYTER_PORT:8080 \
+        -e GIT_USER_NAEM=$GIT_USER_NAME \
+        -e GIE_USER_EMAIL=$GIT_USER_EMAIL \
+        -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+        -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+        -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
+    coding-python-env:$TAG \
+    /bin/zsh
+else
+    docker run --rm -it \
+        -h python-cpu \
+        -p $JUPYTER_PORT:8080 \
+        -e GIT_USER_NAEM=$GIT_USER_NAME \
+        -e GIE_USER_EMAIL=$GIT_USER_EMAIL \
+        -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+        -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+        -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
+    coding-python-env:$TAG \
+    /bin/zsh
+fi
